5c93f7b97fa00895497515eb1c199d429cbc5e0b
NwaguChima <nwaguchima48@gmail.com>
2022-07-24 22:42:57 +0100

    compledted implementation using react query

--- src/Example2.js
+++ src/Example2.js
@@ -1,38 +1,56 @@
-import { useState, useRef, useCallback } from "react";
-import usePosts from "./hooks/usePosts";
+import { useRef, useCallback } from "react";
 import Post from "./Post";
+import { useInfiniteQuery } from "react-query";
+import { getPostsPage } from "./api/axios";
 
 const Example2 = () => {
-  const [pageNum, setPageNum] = useState(1);
-  const { isLoading, isError, error, results, hasNextPage } = usePosts(pageNum);
+  const {
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    data,
+    status,
+    error,
+  } = useInfiniteQuery(
+    "/posts",
+    ({ pageParam = 1 }) => getPostsPage(pageParam),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.length ? allPages.length + 1 : undefined;
+      },
+    }
+  );
 
   const intObserver = useRef();
   const lastPostRef = useCallback(
     (post) => {
-      if (isLoading) return;
+      if (isFetchingNextPage) return;
 
       if (intObserver.current) intObserver.current.disconnect();
 
       intObserver.current = new IntersectionObserver((posts) => {
         if (posts[0].isIntersecting && hasNextPage) {
           console.log("We are near the last post!");
-          setPageNum((prev) => prev + 1);
+          fetchNextPage();
         }
       });
 
       if (post) intObserver.current.observe(post);
     },
-    [isLoading, hasNextPage]
+    [isFetchingNextPage, fetchNextPage, hasNextPage]
   );
 
-  if (isError) return <p className="center">Error: {error.message}</p>;
+  if (status === "error")
+    return <p className="center">Error: {error.message}</p>;
 
-  const content = results.map((post, i) => {
-    if (results.length === i + 1) {
-      return <Post ref={lastPostRef} key={post.id} post={post} />;
-    }
+  const content = data?.pages.map((pg) => {
+    return pg.map((post, i) => {
+      if (pg.length === i + 1) {
+        return <Post ref={lastPostRef} key={post.id} post={post} />;
+      }
 
-    return <Post key={post.id} post={post} />;
+      return <Post key={post.id} post={post} />;
+    });
   });
 
   return (
@@ -40,10 +58,10 @@ const Example2 = () => {
       <h1 id="top">
         &infin; Infinite Query &amp; Scroll
         <br />
-        &infin; Ex. 1 - React Only
+        &infin; Ex. 2 - React Query
       </h1>
       {content}
-      {isLoading && <p className="center">Loading More Posts...</p>}
+      {isFetchingNextPage && <p className="center">Loading More Posts...</p>}
       <p className="center">
         <a href="#top">Back to Top</a>
       </p>
