32f06956afc41ccc07c341243887798557b5a0e3
NwaguChima <nwaguchima48@gmail.com>
2022-07-24 21:51:22 +0100

    seupt usePost hook

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { getPostsPage } from "../api/axios";
+
+const usePosts = (pageNum = 1) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState({});
+  const [hasNextPage, setHasNextPage] = useState(false);
+
+  return { isLoading, isError, error, results, hasNextPage };
+};
+
+export default usePosts;
